d81ffb10c8f540b61cc18fcf2fd2f27fc02f5dce
cachemeifyoucan0 <masonmilly@gmail.com>
2021-02-11 12:03:59 -0500

    adding mammoth to scan .docx to text

--- git-a-job/server.js
+++ git-a-job/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var multer = require('multer')
 var cors = require('cors');
+var mammoth = require("mammoth");
 
 app.use(cors())
 
@@ -10,7 +11,11 @@ var storage = multer.diskStorage({
     cb(null, 'public')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' +file.originalname )
+      //console.log(file.originalname)
+      let extention = (file.originalname).split('.')[1]
+      //console.log(extention) 
+      // every resume will just be uploaded as 'resume.docx to make it easier for initial parsing
+      cb(null, 'resume.' +  extention)
   }
 })
 
@@ -19,11 +24,20 @@ var upload = multer({ storage: storage }).single('file')
 app.post('/upload',function(req, res) {
      
     upload(req, res, function (err) {
+        
            if (err instanceof multer.MulterError) {
                return res.status(500).json(err)
            } else if (err) {
                return res.status(500).json(err)
            }
+           mammoth.extractRawText({path: "public/resume.docx"})
+        .then(function(result){
+            var text = result.value; // The raw text
+            console.log(text)
+            var messages = result.messages;
+        })
+
+           
       return res.status(200).send(req.file)
 
     })
